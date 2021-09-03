c6b3e9cd52d8b05f0e1c8b1f8351a9df3eeff663
Omar <omar@kamelizer.com>
2021-09-03 15:01:09 +0200

    add passport and jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,6 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
@@ -6,6 +8,15 @@ import { UserRepository } from './user.repository';
 
 @Module({
   imports:[
+    PassportModule.register({defaultStrategy:'jwt'}),
+
+    JwtModule.register({
+      secret: 'topSecret51',
+      signOptions: {
+        expiresIn: 3600
+      }
+    }),
+
     TypeOrmModule.forFeature([UserRepository])
   ],
   controllers: [AuthController],
